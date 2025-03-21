b7ef3c8f9e52658070ad965e8c2463a197f1b771
Abd Ur Rab <126180930+abdurrab11@users.noreply.github.com>
2025-03-21 19:00:40 +0500

    Update fb.py

--- fb.py
+++ fb.py
@@ -43,7 +43,7 @@ logo = f"""
 {A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 {Y}Developer : Abd Ur Rab
 {Y}Tool Name : Facebook Cracker
-{Y}Version   : 8.0 (Fully Upgraded)
+{Y}Version   : 9.0 (Fully Fixed)
 {Y}Status    : Active
 {Y}Date      : {current_date}
 {Y}Time      : {current_time}
